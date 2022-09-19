a3a2cd75d5cd56009fff2cb04d789a24be91ba13
dgodin19 <dennis_godin@yahoo.com>
2022-09-19 11:17:46 -0700

    app with boilerplate

---
+++ app.R
@@ -0,0 +1,20 @@
+library(shiny)
+
+#Boilerplate code
+ui <- pageWithSidebar(
+  
+  # App title ----
+  headerPanel("Miles Per Gallon"),
+  
+  # Sidebar panel for inputs ----
+  sidebarPanel(),
+  
+  # Main panel for displaying outputs ----
+  mainPanel()
+)
+
+server <- function(input, output) {
+  
+}
+
+shinyApp(ui, server)
