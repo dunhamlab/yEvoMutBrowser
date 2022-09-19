17133dc015f462718cb2d196f4ba3234c0e0b1ee
dgodin19 <dennis_godin@yahoo.com>
2022-09-19 12:39:49 -0700

    added upload buttons. They don't do anything yet

--- app.R
+++ app.R
@@ -1,14 +1,28 @@
 library(shiny)
+library(DBI)
+library(RSQLite)
 
+# Connect to SQLite database
+con <- dbConnect(RSQLite::SQLite(), "GenomicsDatabase.db")
+
+dbListTables(con)
+
+# Disconnect from database
+
+dbDisconnect(con)
 #Boilerplate code
 ui <- pageWithSidebar(
   
   # App title ----
-  headerPanel("Miles Per Gallon"),
+  headerPanel("Please insert your files into the form below"),
   
   # Sidebar panel for inputs ----
   sidebarPanel(
-    
+    fileInput("file1", "Upload VCF", accept = ".gz"),
+    fileInput("file2", "Upload GFF", accept = ".gff"),
+    fileInput("file3", "Upload FAStA", accept = ".fasta"),
+    actionButton("action", label = "Write to DB"),
+    hr()
     #Input goes here
   ),
   
