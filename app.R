42cc31e18fa3951fe271617cb8cec3978b87c181
dgodin19 <dennis_godin@yahoo.com>
2022-09-20 11:49:50 -0700

    simplified database and shiny

--- app.R
+++ app.R
@@ -19,8 +19,8 @@ ui <- pageWithSidebar(
   # Sidebar panel for inputs ----
   sidebarPanel(
     fileInput("file1", "Upload VCF", accept = ".gz"),
-    fileInput("file2", "Upload GFF", accept = ".gff"),
-    fileInput("file3", "Upload FAStA", accept = ".fasta"),
+    dateInput("date1", "Current Date"),
+    textInput("value1", "Party of Interest"),
     actionButton("action", label = "Write to DB"),
     hr()
     #Input goes here
@@ -35,15 +35,16 @@ ui <- pageWithSidebar(
 )
 
 #Preprocess data here
-
+#Simplified connection
 server <- function(input, output) {
   #Put the actual logic for vcf logic and visualizations here
   data <- eventReactive(input$action, {
     con <- dbConnect(SQLite(), dbname="GenomicsDatabase.db")
-    dbWriteTable(con, "Genome",savemode = "u", data.frame(value1 = input$file1, value2 = input$file2, 
-                                         value3 = input$file3, 
+    dbWriteTable(con, "VCF",savemode = "u", data.frame(value1 = input$file1, 
+                                                          value2 = input$date1, 
+                                         value3 = input$value1, 
                                          stringsAsFactors = FALSE), append = TRUE)
-    data <- dbReadTable(con, "Genome")
+    data <- dbReadTable(con, "VCF")
     dbDisconnect(con)
     return(data)
   })
