3142ac8d942e9ea22c857f8786f55ef90087206f
Leah Anderson <leahmarieanderson@gmail.com>
2025-07-16 17:04:11 -0700

    fixing pie chart colors
    making them match to the gene view lollipop colors

--- R/config.R
+++ R/config.R
@@ -19,10 +19,10 @@ ORGANISM_GENE_INFO_LINK_FUNCTION <- function (genes_info, selected_gene) {
 # COLORS----------------------------------------------------------------------------------------------------------------
 
 VARIANTS_PIE_CHART_COLORS <- c(
-  "#9edae5", "#17becf", "#dbdb8d", "#bcbd22", "#c7c7c7",
-  "#e377c2", "#7f7f7f", "#f7b6d2", "#c49c94", "#8c564b",
-  "#c5b0d5", "#9467bd", "#ff9896", "#d62728", "#98df8a",
-  "#2ca02c", "#ffbb78", "#ff7f0e", "#aec7e8", "#1f77b4"
+  "#F5C710", "#17becf", "#DF536B", "#f7b6d2",  "#c49c94",
+  "#8c564b", "#800080", "#61D04F", "#7f7f7f", "cornflowerblue",
+  "#c5b0d5", "#9467bd", "#ff9896", "#d62728", "#98df8a", "#e377c2",
+  "#2ca02c", "#ffbb78", "#ff7f0e", "#aec7e8", "#1f77b4", "#9edae5"
 )
 SNP_CHART_COLORS <- c("#0072B2", "#CC79A7", "#009E73")
 GENE_VIEW_COLORS <- c("#800080", "#61D04F", "#F5C710", "#DF536B", "#FFB6C1", "cornflowerblue")
