c50c7531efed1f64d4fd649e219844a7c7ee56a5
Eleftheria Anastasia Beres <eberes@uw.edu>
2025-10-21 12:24:19 -0700

    Remove outdated deploy script

--- deploy_app.R
+++
@@ -1,7 +0,0 @@
-library(rsconnect)
-
-rsconnect::deployApp(
-  appDir = "/Users/leahanderson/Documents/GitHub/yEvoMutBrowser",                  # root folder of your app
-  appName = "yEvoMutBrowser2",    # or whatever name you want
-  account = "leahmarieanderson"  # your shinyapps.io account name
-)
